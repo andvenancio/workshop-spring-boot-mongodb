c23777c341f02f6fac903b2d40d6bb6db8c40594
Anderson Quandt Venâncio <andvenancio@gmail.com>
2019-02-11 11:45:23 -0200

    Obtendo um usuário por ID

---
+++ src/main/java/com/andvenancio/workshopmongo/services/UserService.java
@@ -0,0 +1,27 @@
+package com.andvenancio.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andvenancio.workshopmongo.domain.User;
+import com.andvenancio.workshopmongo.repository.UserRepository;
+import com.andvenancio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
